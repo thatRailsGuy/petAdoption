d914b3329cce5f76be3f52638b724e0a60bc8468
Clint Cecil <umdmariachi@gmail.com>
2013-02-21 09:04:28 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PetAdoption::Application.config.secret_token = 'bcbefd9b2d39625b656ea6cf848db1e6a848bb895c345831df4a223b7abe37e5f3a9e974c3c1fec50ff136db808e5162ec3dd629053ac48898c125366b1db416'
